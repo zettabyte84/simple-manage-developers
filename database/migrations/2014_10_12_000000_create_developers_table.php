dc00232cf5be5b8db542dc5a46db7985496b6ef7
zettabyte84 <zettabyte84@gmail.com>
2021-09-21 09:48:49 +0800

    update

--- database/migrations/2014_10_12_000000_create_developers_table.php
+++ database/migrations/2014_10_12_000000_create_developers_table.php
@@ -21,7 +21,7 @@ class CreateDevelopersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone_number');
-            $table->text('address');
+            $table->text('address')->nullable();
             $table->text('avatar');
             $table->rememberToken();
             $table->timestamps();
